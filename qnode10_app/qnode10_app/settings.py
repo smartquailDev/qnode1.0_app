ed3666597e98f6771ef3761f0fcc4082d361802a
smartquailDev <smartquail.dev@gmail.com>
2022-05-17 18:31:06 -0500

    settings

--- qnode10_app/qnode10_app/settings.py
+++ qnode10_app/qnode10_app/settings.py
@@ -82,6 +82,15 @@ INSTALLED_APPS = [
     
 ]
 
+#Email setups
+EMAIL_HOST          = 'smtp.gmail.com'
+EMAIL_PORT          =  587
+EMAIL_HOST_USER     = 'smartquail.info@gmail.com'
+EMAIL_HOST_PASSWORD = 'ms95355672'
+EMAIL_USE_TLS       = True
+DEFAULT_FROM_EMAIL  = EMAIL_HOST_USER
+EMAIL_BACKEND       = 'django.core.mail.backends.smtp.EmailBackend'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
