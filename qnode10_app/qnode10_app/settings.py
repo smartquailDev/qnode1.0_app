c26d630ed1355d368324b5f6e8df8e9d7118cfed
smartquailDev <smartquail.dev@gmail.com>
2022-05-18 01:06:28 -0500

    Update settings.py

--- qnode10_app/qnode10_app/settings.py
+++ qnode10_app/qnode10_app/settings.py
@@ -209,8 +209,9 @@ Configuration.configure(
 )
 
 # celery setup
+CELERY_BROKER_URL = 'amqp://guest:guest@localhost:5672//'
 #CELERY_BROKER_URL = 'amqp://guest:guest@rabbitmq:5672//'
-CELERY_BROKER_URL = 'redis://redis:6379/0'
+#CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://redis:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
